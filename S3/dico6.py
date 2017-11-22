e5cb8facd24cacd9f87b17aba03bbfcc5ea72749
Christian Schweickert <christian.onweb87@gmail.com>
2017-11-22 23:41:07 +0100

    yoyo

--- S3/dico6.py
+++ S3/dico6.py
@@ -1,7 +1,7 @@
 import os
 import re
 import sys
-
+from collections import defaultdict
 
 filename=r"graph1.txt"
 
@@ -10,15 +10,17 @@ def graph_dict(filename):
          with open(filename, "r", encoding='utf-8') as lines:
              try:
                  print("***"*10)
-                 #dico = {}
-                 #pattern = re.compile(r"[\w]{1} [0-9]+ [\w]{1}", re.UNICODE)
-                 pattern = re.compile(r"[\w]{1} [0-9]+ [\w]{1}", re.UNICODE)
+                 dico = defaultdict(list)
+                 the_tuple=()
+                 pattern = re.compile(r"[\w]+", re.UNICODE)
                  for line in lines :
-                     print(line)
-                     sd = pattern.search(line)
-                     print(sd, ', ', type(sd))
-                     print(sd.groupdict())
-                     #dico.append
+                     sd = pattern.findall(line)
+                     the_tuple = tuple(sd) 
+                     #print(sd, ', ', type(sd))
+                     print(the_tuple, ', ', type(the_tuple))
+                     for x, y in the_tuple:
+                        dico[x].append(y)
+                 print(dico)
                  print("***"*10)    
              except IOError as e:
                 print ("I/O error({0}): {1}".format(e.errno, e.strerror))
