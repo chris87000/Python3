b407f89c33c0a638965155167617e0e353a6fd1d
chris87000 <christian.onweb87@gmail.com>
2017-11-24 15:45:31 +0100

    djkdfjdf

---
+++ S3/dico8.py
@@ -0,0 +1,4 @@
+s = set()
+print(type(s))
+     
+s = {1,2,3,'a',True}
---
+++ S3/dico9.py
@@ -0,0 +1,15 @@
+heteroclite = {'marc', 12, 'pierre', (1, 2, 3), 'pierre'}
+print(heteroclite, ", ", type(heteroclite))
+
+heteroclite2 = set(['marc', 12, 'pierre', (1, 2, 3), 'pierre'])
+print(heteroclite2)
+
+print("**"*10)
+ensemble_vide = set()
+print(type(ensemble_vide))
+
+print("**"*10)
+autre_ensemble_vide = set([])
+print(type(autre_ensemble_vide))
+
+print("**"*10)
