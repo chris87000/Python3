8e6248a971606b541683a0cedbce475516b8ce48
chris87000 <christian.onweb87@gmail.com>
2017-11-24 16:20:58 +0100

    eed

--- S3/dico10.py
+++ S3/dico10.py
@@ -1,4 +1,30 @@
 
-#Exercice - niveau basique
+# Exercice - niveau basique
 # read set ...
+#===============================================================================
+# 4615
+#  12
+#  9228
+# 6158
+# 12
+#===============================================================================
 
+import os
+import sys
+from pprint import pprint
+
+filename =r"data/setref1.txt"
+def read_set(filename):
+    with open(extended_file, "r", encoding='utf-8') as lines:
+        try:
+            for line in lines :
+                print("")
+                
+            return 
+        except IOError as e:
+            print ("I/O error({0}): {1}".format(e.errno, e.strerror))
+            print ("Unexpected error:", sys.exc_info()[0])  
+        except: #handle other exceptions such as attribute errors
+            print ("Unexpected error:", sys.exc_info()[0])            
+            print("erreur sur le fichier {}".format(ifilename))
+    return  
