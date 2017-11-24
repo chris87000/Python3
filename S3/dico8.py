00a0ad9363aa31f23ff31ae957272d6f27627a8b
chris87000 <christian.onweb87@gmail.com>
2017-11-24 16:11:34 +0100

    djkdfjdf

--- S3/dico8.py
+++ S3/dico8.py
@@ -2,3 +2,30 @@ s = set()
 print(type(s))
      
 s = {1,2,3,'a',True}
+
+a = [1,2,3,1,18,30,4,1]
+
+be =set(a)
+print(be, ", ", len(be))
+
+if 1 in be:
+    print("OK")
+ 
+s.add('alice')
+
+print(s)
+
+# add only elements which don't appear in the original set
+s.update([1,2,3,4,5,6,6])
+print(s)
+
+s1={1,2,3}
+s2 = {3,4,5}
+print(s1-s2)
+print(s1|s2)     
+print(s1 & s2)
+
+print("**"*10)
+a=[0]
+s = set(a)
+print(s)
--- S3/dico9.py
+++ S3/dico9.py
@@ -13,3 +13,19 @@ autre_ensemble_vide = set([])
 print(type(autre_ensemble_vide))
 
 print("**"*10)
+print(type({}))
+print(set())
+print(set([]))
+
+s = set()
+s = {1,2,3,6,7,9,586,47,64,98}
+p = set()
+p = {11,99,66}
+
+s.add(p)
+print(s)
+
+f = frozenset()
+f = {333,555,999}
+s.add(f)
+print(s)      
